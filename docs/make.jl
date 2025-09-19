d167e9705a9528016bf9e266e54623efe779cf86
Ni Wang <ni.wang@tno.nl>
2025-09-19 10:55:41 +0200

    Fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ DocMeta.setdocmeta!(
     recursive = true,
 )
 
-const page_rename = Dict("developer.md" => "Developer docs") # Without the numbers
+# const page_rename = Dict("developer.md" => "Developer docs") # Without the numbers
 const numbered_pages = [
     file for file in readdir(joinpath(@__DIR__, "src")) if
     file != "index.md" && splitext(file)[2] == ".md"
@@ -17,7 +17,6 @@ const numbered_pages = [
 makedocs(;
     modules = [NearOptimalAlternatives],
     authors = "",
-    repo = "https://github.com/TulipaEnergy/NearOptimalAlternatives.jl/blob/{commit}{path}#{line}",
     sitename = "NearOptimalAlternatives.jl",
     format = Documenter.HTML(;
         canonical = "https://TulipaEnergy.github.io/NearOptimalAlternatives.jl",
