8a397564ca0733bf5b56955d438577831b25c9f3
Ni Wang <ni.wang@tno.nl>
2025-09-19 10:57:39 +0200

    Fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,6 @@ DocMeta.setdocmeta!(
     recursive = true,
 )
 
-# const page_rename = Dict("developer.md" => "Developer docs") # Without the numbers
 const numbered_pages = [
     file for file in readdir(joinpath(@__DIR__, "src")) if
     file != "index.md" && splitext(file)[2] == ".md"
