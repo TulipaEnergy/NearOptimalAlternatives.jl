8a6002a89fe8eb9895fa27830571d5f73f97e1a0
Ni Wang <125902905+gnawin@users.noreply.github.com>
2026-01-20 11:48:28 +0100

    Add psoga (#61)
    * Add psoga
    
    * Fix JuliaFormatter
    
    * Minor module rearrangements
    
    * Further rearrangements

--- src/NearOptimalAlternatives.jl
+++ src/NearOptimalAlternatives.jl
@@ -1,13 +1,17 @@
 module NearOptimalAlternatives
 
-# Packages
-
 using JuMP
 using Distances
 using MathOptInterface
 using Metaheuristics
 using DataStructures
+using Statistics
 
+# PSOGA Algorithm
+include("algorithms/PSOGA/PSOGA.jl")
+include("algorithms/PSOGA/is_better.jl")
+
+# MGA Methods
 include("MGA-Methods/Max-Distance.jl")
 include("MGA-Methods/HSJ.jl")
 include("MGA-Methods/Spores.jl")
@@ -15,11 +19,14 @@ include("MGA-Methods/Min-Max-Variables.jl")
 include("MGA-Methods/Random-Vector.jl")
 include("MGA-Methods/Directionally-Weighted-Variables.jl")
 
-include("results.jl")
-include("alternative-optimization.jl")
+# Main file for generating alternatives
 include("generate-alternatives.jl")
+
+# Create different problems
+include("alternative-optimization.jl")
 include("alternative-metaheuristics.jl")
 
-
+# Update solutions
+include("results.jl")
 
 end
