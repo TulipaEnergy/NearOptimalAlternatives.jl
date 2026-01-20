f57b9cfa6fce8ff11db44d01bcf4f07cb82abd22
Ni Wang <ni.wang@tno.nl>
2026-01-20 11:41:55 +0100

    Minor module rearrangements

--- src/NearOptimalAlternatives.jl
+++ src/NearOptimalAlternatives.jl
@@ -9,19 +9,26 @@ using Metaheuristics
 using DataStructures
 using Statistics
 
+# Main file for generating alternatives
+include("generate-alternatives.jl")
+
+# Create different problems
+include("alternative-optimization.jl")
+include("alternative-metaheuristics.jl")
+
+# Update solutions
+include("results.jl")
+
+# PSOGA Algorithm
+include("algorithms/PSOGA/PSOGA.jl")
+include("algorithms/PSOGA/is_better.jl")
+
+# MGA Methods
 include("MGA-Methods/Max-Distance.jl")
 include("MGA-Methods/HSJ.jl")
 include("MGA-Methods/Spores.jl")
 include("MGA-Methods/Min-Max-Variables.jl")
 include("MGA-Methods/Random-Vector.jl")
 include("MGA-Methods/Directionally-Weighted-Variables.jl")
-
-include("results.jl")
-include("alternative-optimization.jl")
-include("generate-alternatives.jl")
-include("alternative-metaheuristics.jl")
-include("algorithms/PSOGA/PSOGA.jl")
-include("algorithms/PSOGA/is_better.jl")
-
 
 end
